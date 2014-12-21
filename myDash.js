f048c39c520697e963cb0f2abde2dd773e11b98d
Michele <micxgx@gmail.com>
2014-12-21 00:33:25 +0000

    myDash: handle horizontal drag and drop in rtl.

--- myDash.js
+++ myDash.js
@@ -705,6 +705,12 @@ const myDash = new Lang.Class({
             pos = numChildren;
         }
 
+        /* Take into account childredn position in rtl*/
+        if (this._isHorizontal &&
+          Clutter.get_default_text_direction() == Clutter.TextDirection.RTL
+          )
+            pos = numChildren - pos;
+
         if (pos != this._dragPlaceholderPos && pos <= numFavorites && this._animatingPlaceholdersCount == 0) {
             this._dragPlaceholderPos = pos;
 
